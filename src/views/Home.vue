4a66689281c7b96ac0f61650279d386d4f00bf2a
thomaszampa <thomas.zampa@gmail.com>
2018-12-03 21:28:54 -0600

    Basic PDF template w/o style, formatt, or backend data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,7 +21,17 @@ export default {
     createPDF() {
       var doc = new jsPDF();
 
-      doc.text("Hello world!", 10, 10);
+      doc.text("Photo", 10, 10);
+      doc.text("First Name", 10, 20);
+      doc.text("Last Name", 10, 20);
+      doc.text("Email", 10, 30);
+      doc.text("Phone Number", 10, 40);
+      doc.text("Short Bio", 50, 100);
+      doc.text("Linkedin URL", 10, 200);
+      doc.text("Online Resume URL", 10, 200);
+      doc.text("Twitter Handle", 10, 200);
+      doc.text("Personal Blog/Website URL", 10, 200);
+      doc.text("GitHub URL", 10, 200);
       doc.save("a4.pdf");
     }
   },
